60b251004fb81c7d0501ed0be731b4bd80194dcb
Sofia Podolsky <spodolskaya@datagrok.ai>
2022-10-25 12:50:22 +0300

    Logs: Feat: Add 7 day expiration for S3 logs

--- aws/s3.tf
+++ aws/s3.tf
@@ -15,6 +15,16 @@ module "log_bucket" {
   attach_lb_log_delivery_policy         = true
   attach_deny_insecure_transport_policy = true
   attach_require_latest_tls_policy      = true
+
+  lifecycle_rule = [
+    {
+      enabled    = true
+      expiration = {
+        days = 7
+      }
+    }
+  ]
+  tags = local.tags
 }
 data "aws_iam_policy_document" "bucket_policy" {
   statement {
