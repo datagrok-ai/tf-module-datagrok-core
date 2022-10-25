1c8494ba575a27dd01652f0a63506300ca77aad4
Sofia Podolsky <spodolskaya@datagrok.ai>
2022-10-25 11:38:54 +0300

    Datagrok: Feat: Add Docker Hub Secret ARN to outputs

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -77,3 +77,8 @@ output "sns_topic" {
   description = "The ARN of the SNS topic from which messages will be sent"
   value       = try(module.sns_topic.sns_topic_arn, var.monitoring_sns_topic_arn)
 }
+
+output "docker_hub_secret" {
+  description = "The ARN of the Secret for Docker Hub Authorisation"
+  value       = try(aws_secretsmanager_secret.docker_hub[0].arn, var.docker_hub_secret_arn)
+}
