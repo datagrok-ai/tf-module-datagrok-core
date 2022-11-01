2e36b0d0ef1eaa76e55cf7860c6a0d2dea26b766
dnillovna <dnillovna@users.noreply.github.com>
2022-11-01 20:36:47 +0000

    GitHub Actions: Refactor: Automated formatting of terraform code

--- aws/lb.tf
+++ aws/lb.tf
@@ -154,7 +154,7 @@ module "lb_int" {
   name                       = "${local.lb_name}-int"
   load_balancer_type         = "application"
   vpc_id                     = try(module.vpc[0].vpc_id, var.vpc_id)
-  subnets                    = try( module.vpc[0].private_subnets, var.private_subnet_ids)
+  subnets                    = try(module.vpc[0].private_subnets, var.private_subnet_ids)
   security_groups            = [module.lb_int_sg.security_group_id]
   drop_invalid_header_fields = true
 
