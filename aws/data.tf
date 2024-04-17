b80e1c2799702d6c872bb53f01e21c67699fad5d
VHlushchen <VHlushchen@users.noreply.github.com>
2024-04-17 10:16:40 +0000

    GitHub Actions: Refactor: Automated formatting of terraform code

--- aws/data.tf
+++ aws/data.tf
@@ -3,5 +3,5 @@ data "aws_kms_key" "backup_vault_kms" {
 }
 
 data "aws_iam_policy" "backup_default_policy" {
-    name = "AWSBackupServiceRolePolicyForBackup"
+  name = "AWSBackupServiceRolePolicyForBackup"
 }
