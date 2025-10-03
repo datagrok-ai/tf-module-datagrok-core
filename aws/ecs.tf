e16edf137685a54297571c7907abb1e395adb2ea
Alex Paramonov <alex.aprm@Gmail.com>
2025-10-03 19:24:11 +0100

    DB SSL support

--- aws/ecs.tf
+++ aws/ecs.tf
@@ -341,7 +341,7 @@ resource "aws_ecs_task_definition" "datagrok" {
                   dbPassword : try(random_password.db_datagrok_password[0].result, var.rds_dg_password),
                   dbAdminLogin : var.rds_master_username,
                   dbAdminPassword : module.db.db_instance_password,
-                  dbSsl : false,
+                  dbSsl : true,
                   deployDemo : false,
                   deployTestDemo : false
                   }, var.set_admin_password ? {
