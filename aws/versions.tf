4e80bac9d10cf1e77f1811a1b5f715e93e2bc66b
Anatol <anatol1988@gmail.com>
2025-11-16 13:31:16 +0100

    Delete resources, provided by CloudFormation already

--- aws/versions.tf
+++ aws/versions.tf
@@ -2,17 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.4.3"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.2.0"
+      version = ">= 6.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.13.0"
 }
